dc00e3bcccf717accac6fbf89464c423dcce4ec6
Ryan Lee <ryantlee9@gmail.com>
2017-09-26 17:34:24 -0700

    Add config extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_restful import Api
 from .links import Link
+from os import getenv
 
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
+app.config.from_object('config.{}'.format(getenv('CONFIG')))
 api = Api(app)
 api.add_resource(Link, "/link")
--- config.py
+++ config.py
@@ -3,6 +3,26 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    DEBUG = os.getenv('DEBUG') == 'True'
-    TESTING = os.getenv('TESTING') == 'True'
-    CSRF_ENABLED = os.getenv('CSRF_ENABLED') == 'True'
+    DEBUG = True
+    TESTING = True
+    CSRF_ENABLED = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('POSTGRES_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
