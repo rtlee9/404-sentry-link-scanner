41722dafb76495d9afd3bee7beee7d9254f0ff63
Ryan Lee <ryantlee9@gmail.com>
2017-09-27 15:17:54 -0700

    Add automatic SQLAlchemy rollbacks

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,15 @@ from os import getenv
 
 app = Flask(__name__)
 app.config.from_object('config.{}'.format(getenv('CONFIG')))
+
+# SQLAlchemy config
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+@app.teardown_request
+def teardown_request(exception):
+    if exception:
+        db.session.rollback()
+        db.session.remove()
+    db.session.remove()
 
 from . import models, api
