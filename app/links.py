cd6b0c59ce7682a62b9517960d5c3b651901134d
Ryan Lee <ryantlee9@gmail.com>
2017-09-28 17:30:49 -0700

    Add schedule get endpoint

--- app/links.py
+++ app/links.py
@@ -2,6 +2,7 @@ from flask_restful import Resource, reqparse
 from flask import request
 from requests import get
 from .link_check import LinkChecker, scheduled_scan
+from . import scheduler
 
 
 class Link(Resource):
@@ -20,3 +21,10 @@ class ScanJob(Resource):
         args = parser.parse_args()
         cron_params = request.get_json()
         scheduled_scan(args.url, cron_params)
+
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('url', type=str, help='URL to check')
+        args = parser.parse_args()
+        job = scheduler.get_job(args.url)
+        return(str(job))
