c5e5eddefd0d0dc367e0f1934b73665f60174799
Ryan Lee <ryantlee9@gmail.com>
2017-10-01 16:30:50 -0700

    Consolidate resources: get scheduled jobs

--- app/api.py
+++ app/api.py
@@ -54,32 +54,6 @@ class HistoricalResults(Resource):
             results=[result.to_json() for result in last_job_results])
 
 
-class ScheduledJobs(Resource):
-    def get(self):
-        """List scheduled jobs for a given user.
-        Optionally, specify a root URL and/or owner to filter results
-        """
-        parser = reqparse.RequestParser()
-        parser.add_argument('url', type=str, help='URL to check')
-        parser.add_argument('owner', type=str, help='Scan job owner')
-        args = parser.parse_args()
-        if (not args.owner) or (not g.user.admin):
-            owner = g.user.username
-        else:
-            owner = args.owner
-        jobs = ScheduledJob.query.filter(ScheduledJob.user == g.user).\
-            \filter(ScheduledJob.owner == owner)
-        if args.url:
-            jobs = jobs.filter(ScheduledJob.root_url == args.url)
-        scheduled_jobs = filter(
-            lambda x: x is not None,
-            [scheduler.get_job(str(job.id)) for job in jobs])
-        return jsonify([
-            dict(next_run_time=job.next_run_time, trigger=str(job.trigger))
-            for job in scheduled_jobs
-        ])
-
-
 class HistoricalJobs(Resource):
     def get(self):
         """List all historical jobs for a given user.
@@ -136,22 +110,30 @@ class LinkScanJob(Resource):
             return response
 
     def get(self):
-        """Get information about a recurring scan job"""
+        """List scheduled jobs for a given user.
+        Optionally, specify a root URL and/or owner to filter results
+        """
         parser = reqparse.RequestParser()
-        parser.add_argument('url', required=True, type=str, help='URL to check')
+        parser.add_argument('url', type=str, help='URL to check')
         parser.add_argument('owner', type=str, help='Scan job owner')
         args = parser.parse_args()
         if (not args.owner) or (not g.user.admin):
             owner = g.user.username
         else:
             owner = args.owner
-        job_records = ScheduledJob.query.\
+        jobs = ScheduledJob.query.\
             filter(ScheduledJob.user == g.user).\
             filter(ScheduledJob.owner == owner)
         if args.url:
-            job_records = job_records.filter(ScheduledJob.root_url == args.url)
-        job = scheduler.get_job(job_records.first())
-        return jsonify(job_id=job.id)
+            jobs = jobs.filter(ScheduledJob.root_url == args.url)
+        scheduled_jobs = filter(
+            lambda x: x is not None,
+            [scheduler.get_job(str(job.id)) for job in jobs])
+        return jsonify([
+            dict(next_run_time=job.next_run_time, trigger=str(job.trigger))
+            for job in scheduled_jobs
+        ])
+
 
 class UrlPermissions(Resource):
     def post(self):
@@ -189,5 +171,4 @@ api.add_resource(LinkScan, "/link-scan")
 api.add_resource(HistoricalJobs, "/jobs/historical")
 api.add_resource(HistoricalResults, "/results/historical")
 api.add_resource(LinkScanJob, "/link-scan/schedule")
-api.add_resource(ScheduledJobs, "/jobs/scheduled")
 api.add_resource(UrlPermissions, "/permissions")
