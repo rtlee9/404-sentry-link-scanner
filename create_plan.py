126ba33ace3db21c4e9ab3dd7b615407f12d9588
Ryan Lee <ryantlee9@gmail.com>
2017-10-12 17:39:01 -0700

    Add trial period arg to create_plan

--- create_plan.py
+++ create_plan.py
@@ -20,17 +20,20 @@ if __name__ == '__main__':
         '-a', '--amount', type=int, help='Payment amount', required=True)
     parser.add_argument(
         '-i', '--interval-count', type=int, help='Interval count', default=1)
+    parser.add_argument(
+        '-t', '--trial-period', type=int, help='Number of days of trial period', default=None)
     args = parser.parse_args()
     plan_id = args.id if args.id else args.name.replace(' ', '_').strip().lower()
 
     # add subscription plan
     stripe.api_key = getenv("STRIPE_API_KEY")
     plan = stripe.Plan.create(
-          name=args.name,
-          id=plan_id,
-          interval=args.interval,
-          interval_count=args.interval_count,
-          currency=args.currency,
-          amount=args.amount,
+        name=args.name,
+        id=plan_id,
+        interval=args.interval,
+        interval_count=args.interval_count,
+        currency=args.currency,
+        amount=args.amount,
+        trial_period_days=args.trial_period,
     )
     print(plan)
