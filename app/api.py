e382cfffdd527bdb2abe10a4b05e6b283142814b
Ryan Lee <ryantlee9@gmail.com>
2017-10-31 17:45:44 -0700

    Refactor auth

--- app/api.py
+++ app/api.py
@@ -1,7 +1,6 @@
 from flask import request, g
 from flask.json import jsonify
 from flask_restful import Api, Resource, reqparse, inputs
-from flask_httpauth import HTTPBasicAuth
 from requests import get
 import datetime
 from apscheduler.jobstores.base import ConflictingIdError
@@ -11,17 +10,7 @@ from .models import User, ScanJob, LinkCheck, ScheduledJob, PermissionedURL, Own
 from . import app, scheduler, db
 from .link_check import LinkChecker, standardize_descheme_url
 from .email import send_email
-
-
-# auth setup
-auth = HTTPBasicAuth()
-@auth.verify_password
-def verify_password(username, password):
-    user = User.query.filter_by(username=username).first()
-    if not user or not user.verify_password(password):
-        return False
-    g.user = user
-    return True
+from .auth import auth
 
 
 def email_results(job):
---
+++ app/auth.py
@@ -0,0 +1,14 @@
+from flask import g
+from flask_httpauth import HTTPBasicAuth
+from .models import User
+
+
+# auth setup
+auth = HTTPBasicAuth()
+@auth.verify_password
+def verify_password(username, password):
+    user = User.query.filter_by(username=username).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.user = user
+    return True
