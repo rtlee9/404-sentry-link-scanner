b9ec535578ad5265679e31fcbdc81e5434515709
Ryan Lee <ryantlee9@gmail.com>
2017-11-02 23:47:33 -0700

    Use mock patching for testing

--- test_link_check.py
+++ test_link_check.py
@@ -1,5 +1,6 @@
 from app.link_check import *
 from app.models import Owner
+from unittest.mock import patch
 
 
 class TestLinkCheck(object):
@@ -10,6 +11,29 @@ class TestLinkCheck(object):
             self.owner.user,
             self.owner
         )
+        self.sample_html = """
+        <HTML>
+        <HEAD>
+        <TITLE>Your Title Here</TITLE>
+        </HEAD>
+        <BODY BGCOLOR="FFFFFF">
+        <CENTER><IMG SRC="clouds.jpg" ALIGN="BOTTOM"> </CENTER>
+        <HR>
+        <a href="http://somegreatsite.com">Link Name</a>
+        <a href="https://blog.dummy.com/internal-link1">Link Name</a>
+        <a href="https://blog.dummy.com/internal-link2">Link Name</a>
+        is a link to another nifty site
+        <H1>This is a Header</H1>
+        <H2>This is a Medium Header</H2>
+        Send me mail at <a href="mailto:support@yourcompany.com">
+        support@yourcompany.com</a>.
+        <P> This is a new paragraph!
+        <P> <B>This is a new paragraph!</B>
+        <BR> <B><I>This is a new sentence without a paragraph break, in bold italics.</I></B>
+        <HR>
+        </BODY>
+        </HTML>
+        """
 
     def test_flat_follow(self):
         test_checker = LinkChecker(
@@ -41,21 +65,27 @@ class TestLinkCheck(object):
         assert r.response is None
         assert r.exception == "ConnectTimeout"
 
-    def test_links_checked_and_followed_eightportions(self):
+    @patch('app.link_check.requests.get')
+    def test_links_checked_and_followed_single_page(self, mock_get):
+        mock_get.return_value.status_code = 404
+        mock_get.return_value.content = self.sample_html
         test_checker = LinkChecker(
-            'https://eightportions.com/datasets',
+            'https://blog.dummy.com',
             self.owner.user,
             self.owner)
         test_checker.check_all_links_and_follow()
-        assert len(test_checker.links_checked_and_followed) > 2
+        assert len(test_checker.links_checked_and_followed) == 3
 
-    def test_links_checked_and_followed_noschema(self):
+    @patch('app.link_check.requests.get')
+    def test_links_checked_and_followed_single_page_no_schema(self, mock_get):
+        mock_get.return_value.status_code = 404
+        mock_get.return_value.content = self.sample_html
         test_checker = LinkChecker(
-            'www.lemonade.com/blog',
+            'blog.dummy.com',
             self.owner.user,
             self.owner)
         test_checker.check_all_links_and_follow()
-        assert len(test_checker.links_checked_and_followed) > 2
+        assert len(test_checker.links_checked_and_followed) == 3
 
     def test_long_response_siafoo(self):
         r = self.test_checker.check_link('http://www.siafoo.net/article/52')
